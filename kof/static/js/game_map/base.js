be3a337d44a643754b5f594375053f6478a9d0ce
yfh <787004055@qq.com>
2024-05-11 21:07:20 +0800

    kof-ultimate

--- kof/static/js/game_map/base.js
+++ kof/static/js/game_map/base.js
@@ -12,6 +12,14 @@ class  GameMap extends AcGameObject{
 
         this.controller=new Controller(this.$canvas);
 
+        this.root.$kof.append($(`<div class="kof-head">
+        <div class="kof-head-hp-0"><div><div></div></div></div>
+        <div class="kof-head-timer">60</div>
+        <div class="kof-head-hp-1"><div><div></div></div></div>
+    </div>`));
+
+        this.time_left = 60000;  // 单位：毫秒
+        this.$timer = this.root.$kof.find(".kof-head-timer");
     }
 
     start(){
@@ -19,6 +27,20 @@ class  GameMap extends AcGameObject{
     }
 
     update() {
+        this.time_left -= this.timedelta;
+        if (this.time_left < 0) {
+            this.time_left = 0;
+
+            let [a, b] = this.root.players;
+            if (a.status !== 6 && b.status !== 6) {
+                a.status = b.status = 6;
+                a.frame_current_cnt = b.frame_current_cnt = 0;
+                a.vx = b.vx = 0;
+            }
+        }
+
+        this.$timer.text(parseInt(this.time_left / 1000));
+
         this.render();
     }
 
--- kof/static/js/player/base.js
+++ kof/static/js/player/base.js
@@ -28,6 +28,9 @@ class Player extends AcGameObject{
         this.animations=new Map();
 
         this.frame_current_cnt = 0;
+        this.hp=100;
+        this.$hp=this.root.$kof.find(`.kof-head-hp-${this.id}>div`);
+        this.$hp_div = this.$hp.find('div');
     }
 
     start(){
@@ -35,6 +38,8 @@ class Player extends AcGameObject{
     }
 
     update_direction(){
+        if(this.status===6)
+            return;
         let players=this.root.players;
         if(players[0]&& players[1]){
             let me=this,you=players[1-this.id];
@@ -45,7 +50,68 @@ class Player extends AcGameObject{
             }
         }
     }
+    is_collision(r1, r2){
+        if(Math.max(r1.x1, r2.x1) > Math.min(r1.x2, r2.x2)){
+            return false;
+        }
+        if(Math.max(r1.y1,r2.y1)>Math.min(r1.y2,r2.y2)){
+            return false;
+        }
+        return true;
+    }
 
+    is_attack(){
+        if(this.status===6)
+            return;
+        this.status=5;
+        this.frame_current_cnt=0;
+        this.hp=Math.max(this.hp-20,0);
+        this.$hp_div.animate({
+            width: this.$hp.parent().width() * this.hp / 100
+        }, 300);
+        this.$hp.animate({
+            width: this.$hp.parent().width() * this.hp / 100
+        }, 600);
+        if(this.hp<=0){
+            this.status=6;
+            this.frame_current_cnt = 0;
+            this.vx = 0;
+        }
+    }
+
+    update_attack(){
+        if(this.status===4 && this.frame_current_cnt===18){
+            let me=this,you=this.root.players[1-this.id];
+            let r1;
+            if(this.direction>0){
+                r1 = {
+                    x1: me.x + 120,
+                    y1: me.y + 40,
+                    x2: me.x + 120 + 100,
+                    y2: me.y + 40 + 20,
+                };
+            }else{
+                r1 = {
+                    x1: me.x + me.width - 120 - 100,
+                    y1: me.y + 40,
+                    x2: me.x + me.width - 120 - 100 + 100,
+                    y2: me.y + 40 + 20,
+                };
+            }
+
+            let r2 = {
+                x1: you.x,
+                y1: you.y,
+                x2: you.x + you.width,
+                y2: you.y + you.height
+            };
+
+            if (this.is_collision(r1, r2)) {
+                you.is_attack();
+            }
+
+        }
+    }
     update_control(){
          let w,a,d,space;
          if(this.id===0){
@@ -91,15 +157,16 @@ class Player extends AcGameObject{
     }
 
     update_move(){
-        if(this.status===3)
-            this.vy+=this.gravity;
+
+        this.vy+=this.gravity;
 
         this.x+=this.vx*this.timedelta /1000 ;
         this.y+=this.vy*this.timedelta/1000;
         if(this.y>450){
             this.y=450;
             this.vy=0;
-            this.status=0;
+            if(this.status===3)
+                this.status=0;
         }
         if(this.x<0){
             this.x=0;
@@ -112,13 +179,22 @@ class Player extends AcGameObject{
         this.update_control();
         this.update_move();
         this.update_direction();
+        this.update_attack();
         this.render();
 
     }
 
     render(){
-        // this.ctx.fillStyle=this.color;
+        // this.ctx.fillStyle='blue';
         // this.ctx.fillRect(this.x,this.y,this.width,this.height);
+        //
+        // if(this.direction>0){
+        //     this.ctx.fillStyle='red';
+        //     this.ctx.fillRect(this.x+200,this.y+40,20,20);
+        // }else{
+        //     this.ctx.fillStyle='red';
+        //     this.ctx.fillRect(this.x+this.width-220,this.y+40,20,20);
+        // }
         let status=this.status;
 
         if(this.status===1 && this.vx * this.direction <0)//判断后退
@@ -145,13 +221,21 @@ class Player extends AcGameObject{
 
 
         }
-        if(status===4 || status===3){
-            if( this.frame_current_cnt == obj.frame_rate * (obj.frame_cnt - 1))
-                this.status=0;
+        if(status===4 || status===5 || status===6){
+            if( this.frame_current_cnt == obj.frame_rate * (obj.frame_cnt - 1)) {
+                if (status === 6) {
+                    this.frame_current_cnt--;
+                } else {
+                    this.status = 0;
+                }
+            }
+
         }
 
         this.frame_current_cnt++;
     }
+
+
 }
 
 export {
